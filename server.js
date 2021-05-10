9b7cba929ebfd6232abd3b7b8e60dcc15499a0de
Kevin <01rayo2015@gmail.com>
2021-05-10 16:11:26 -0700

    update

--- server.js
+++ server.js
@@ -19,12 +19,20 @@ const sess = {
 	}),
 };
 app.use(session(sess));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+
 app.use(routes);
-sequelize.sync({ force: false }).then(() => {
-	app.listen(PORT, () => console.log("Now listening on port: " + PORT));
+// sequelize.sync({ force: false }).then(() => {
+// 	app.listen(PORT, () => console.log("Now listening on port: " + PORT));
+// });
+app.listen(PORT, () => {
+	console.log("Now listening on port: " + PORT);
+	sequelize.sync({ force: false })
 });
